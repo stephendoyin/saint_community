0499a292fa448cf4cf2c7fbd98809af6cbcd83ec
clevmania <lawrence@tru-data.com>
2019-03-20 10:55:24 +0100

    update

--- resources/js/contact.js
+++ resources/js/contact.js
@@ -2,26 +2,32 @@
 let modalTriggerBtn = document.querySelector(".js_modal_trigger__btn");
 let closeBtn = document.querySelector(".modal_contact__close");
 let contactModal = document.querySelector(".contact_modal");
+let innerModal = document.querySelector(".contact_modal_inner");
+contactModal.style.height = "1500px";
+contactModal.style.overflow = "auto";
 
-
-modalTriggerBtn.addEventListener("click", function(e){
+modalTriggerBtn.addEventListener("click", function (e) {
     e.preventDefault();
-    document.body.classList.add("body");
+    document.body.style.height = "100vh";
+    document.body.style.overflow = "hidden";
     toggleModal();
+
 });
 
 
-closeBtn.addEventListener("click", function(){
+closeBtn.addEventListener("click", function () {
     toggleModal();
-    document.body.classList.remove("body");
+    document.body.style.height = "auto";
+    document.body.style.overflow = "auto";
 
 });
 
-window.addEventListener("click", function(e){
-    if(e.target === contactModal)
+window.addEventListener("click", function (e) {
+    if (e.target === innerModal){
         toggleModal();
-        document.body.classList.remove("body");
-
+        document.body.style.height = "auto";
+        document.body.style.overflow = "auto";
+    }
 })
 
 function toggleModal() {
