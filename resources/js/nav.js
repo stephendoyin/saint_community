cc920e89a1d2a28d2b5a0153b1bd559f0f64a2c2
clevmania <lawrence@tru-data.com>
2019-03-20 16:34:40 +0100

    update

--- resources/js/nav.js
+++ resources/js/nav.js
@@ -3,15 +3,21 @@ let menuModal = $(".nav_menu__resp");
 
 
 $(".menu__close").click(function(){
-    menuModal.animate({left: "-100%"})
+    menuModal.animate({left: "-100%"});
+    document.body.style.height = "auto";
+    document.body.style.overflow = "auto";
 })
 
 $(window).click(function(e) {
     if(e.target.className === "nav_menu__resp"){
-        menuModal.animate({left: "-100%"})
+        menuModal.animate({left: "-100%"});
+        document.body.style.height = "auto";
+    document.body.style.overflow = "auto";
     }
 });
 
 menuIcon.click(function(){
     menuModal.animate({left: "0%"});
+    document.body.style.height = "100vh";
+    document.body.style.overflow = "hidden";
 })
