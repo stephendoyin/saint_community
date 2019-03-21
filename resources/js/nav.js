83027d418f7e16328e84441be7585e8c2945127d
clevmania <lawrence@tru-data.com>
2019-03-21 15:16:11 +0100

    update

--- resources/js/nav.js
+++ resources/js/nav.js
@@ -20,4 +20,17 @@ menuIcon.click(function(){
     menuModal.animate({left: "0%"});
     document.body.style.height = "100vh";
     document.body.style.overflow = "hidden";
-})
+});
+
+function colorRed() {
+
+    for(let i = 0; i < document.querySelectorAll(".resp_menu__link--active:link path").length; i++){
+        document.querySelectorAll(".resp_menu__link--active:link path")[i].setAttribute("fill", "#f00");
+        if (document.querySelectorAll(".resp_menu__link--active:link polygon").length > 0){
+            document.querySelectorAll(".resp_menu__link--active:link polygon")[i].setAttribute("fill", "#f00");
+        }
+    }
+    
+}
+
+colorRed();
